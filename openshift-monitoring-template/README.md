572f779dfd3dcf9f9befb02578c74b083d8b2053
Sebastian Dehn <sdehn@redhat.com>
2019-02-06 16:31:15 +0100

    Added install script to discover token for Prometheus connection and put it into the template file.

---
+++ openshift-monitoring-template/README.md
@@ -0,0 +1,12 @@
+# Grafana with OpenShift OAuth Proxy
+
+## NOTE
+You have to be cluster-admin to use install the Grafana template
+
+## Usage
+
+```
+chmod +x installTemplate.sh
+./install.sh $YOUR_OCP_USER $YOUR_OCP_MASTER_URL
+```
+
---
+++ openshift-monitoring-template/installTemplate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERNAME=$1
+URL=$2
+
+if [ -z "$USERNAME" ]
+then
+    echo "Please provide your OCP user"
+    exit 0
+fi
+
+if [ -z "$URL" ]
+then
+    echo "Please provide an OCP host"
+    exit 0
+fi
+
+oc login -u $USERNAME $URL
+
+OAUTHKEY=$(oc export secret grafana-datasources -n openshift-monitoring -o json | jq -r '.data."prometheus.yaml"' | base64 --decode | jq -r '.datasources[].basicAuthPassword')
+sed -i.bak 's#basicAuthPassword":"#basicAuthPassword":"'"$OAUTHKEY"'#' grafana-template.yaml
+
+oc create -f grafana-template.yaml.bak
+rm -f *.bak
+echo "Finish"
